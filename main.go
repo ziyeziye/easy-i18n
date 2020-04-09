0f7f8d9a85374f03cee81a2d84ceb9e569bb16f9
Lukin <mylukin@gmail.com>
2020-04-09 15:41:10 +0800

    update

--- main.go
+++ main.go
@@ -3,31 +3,31 @@ package main
 import (
 	"fmt"
 
-	goi18n "github.com/mylukin/easy-i18n/i18n"
+	"github.com/mylukin/easy-i18n/i18n"
 )
 
 func main() {
 
 	test("a")
 
-	goi18n.Printf("hello world!")
+	i18n.Printf("hello world!")
 	fmt.Println()
 
 	name := "Lukin"
 
-	goi18n.Printf("hello %s!", name)
+	i18n.Printf("hello %s!", name)
 	fmt.Println()
 
-	goi18n.Printf("%s has %d apple.", name, 1)
+	i18n.Printf("%s has %d apple.", name, 1)
 	fmt.Println()
 
-	goi18n.Printf("%s has %d cat.", name, 2, goi18n.Plural(
+	i18n.Printf("%s has %d cat.", name, 2, i18n.Plural(
 		"%[2]d=1", "%s has %d cat.",
 		"%[2]d>1", "%s has %d cats.",
 	))
 	fmt.Println()
 
-	goi18n.Extract([]string{
+	i18n.Extract([]string{
 		".",
 	}, "./en.json")
 }
