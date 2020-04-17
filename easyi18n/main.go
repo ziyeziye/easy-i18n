6c6db8bb2aaf57f94a3367d2b6973fd4aa761a33
Lukin <mylukin@gmail.com>
2020-04-17 11:23:49 +0800

    update

--- easyi18n/main.go
+++ easyi18n/main.go
@@ -27,7 +27,7 @@ func main() {
 	app := &cli.App{
 		HelpName: appName,
 		Name:     appName,
-		Usage:    i18n.Sprintf("a tool for managing message translations."),
+		Usage:    i18n.Sprintf(`a tool for managing message translations.`),
 		Action: func(c *cli.Context) error {
 			cli.ShowAppHelp(c)
 			return nil
@@ -37,17 +37,17 @@ func main() {
 			{
 				Name:      "update",
 				Aliases:   []string{"u"},
-				Usage:     i18n.Sprintf("merge translations and generate catalog"),
-				UsageText: i18n.Sprintf("%s update srcfile destfile", appName),
+				Usage:     i18n.Sprintf(`merge translations and generate catalog`),
+				UsageText: i18n.Sprintf(`%s update srcfile destfile`, appName),
 				Action: func(c *cli.Context) error {
 					srcFile := c.Args().Get(0)
 					if len(srcFile) == 0 {
-						return fmt.Errorf(i18n.Sprintf("srcfile cannot be empty"))
+						return fmt.Errorf(i18n.Sprintf(`srcfile cannot be empty`))
 					}
 
 					destFile := c.Args().Get(1)
 					if len(destFile) == 0 {
-						return fmt.Errorf(i18n.Sprintf("destfile cannot be empty"))
+						return fmt.Errorf(i18n.Sprintf(`destfile cannot be empty`))
 					}
 
 					err := i18n.Update(srcFile, destFile)
@@ -58,8 +58,8 @@ func main() {
 			{
 				Name:      "extract",
 				Aliases:   []string{"e"},
-				Usage:     i18n.Sprintf("extracts strings to be translated from code"),
-				UsageText: i18n.Sprintf("%s extract [path] [outfile]", appName),
+				Usage:     i18n.Sprintf(`extracts strings to be translated from code`),
+				UsageText: i18n.Sprintf(`%s extract [path] [outfile]`, appName),
 				Action: func(c *cli.Context) error {
 					path := c.Args().Get(0)
 					if len(path) == 0 {
@@ -78,13 +78,13 @@ func main() {
 			{
 				Name:      "generate",
 				Aliases:   []string{"g"},
-				Usage:     i18n.Sprintf("generates code to insert translated messages"),
-				UsageText: i18n.Sprintf("%s generate [path] [outfile]", appName),
+				Usage:     i18n.Sprintf(`generates code to insert translated messages`),
+				UsageText: i18n.Sprintf(`%s generate [path] [outfile]`, appName),
 				Flags: []cli.Flag{
 					&cli.StringFlag{
 						Name:  "pkg",
 						Value: "main",
-						Usage: i18n.Sprintf("generated go file package name"),
+						Usage: i18n.Sprintf(`generated go file package name`),
 					},
 				},
 				Action: func(c *cli.Context) error {
--- example/main.go
+++ example/main.go
@@ -17,27 +17,27 @@ func main() {
 
 	i18n.SetLang(language.SimplifiedChinese)
 
-	i18n.Printf("hello world!")
+	i18n.Printf(`hello world!`)
 	fmt.Println()
 
-	name := "Lukin"
+	name := `Lukin`
 
-	i18n.Printf("hello %s!", name)
+	i18n.Printf(`hello %s!`, name)
 	fmt.Println()
 
-	i18n.Printf("%s has %d cat.", name, 1)
+	i18n.Printf(`%s has %d cat.`, name, 1)
 	fmt.Println()
 
-	i18n.Printf("%s has %d cat.", name, 2, i18n.Plural(
-		"%[2]d=1", "%s has %d cat.",
-		"%[2]d>1", "%s has %d cats.",
+	i18n.Printf(`%s has %d cat.`, name, 2, i18n.Plural(
+		`%[2]d=1`, `%s has %d cat.`,
+		`%[2]d>1`, `%s has %d cats.`,
 	))
 	fmt.Println()
 
-	i18n.Fprintf(os.Stderr, "%s have %d apple.", name, 2, i18n.Plural(
-		"%[2]d=1", "%s have an apple.",
-		"%[2]d=2", "%s have two apples.",
-		"%[2]d>2", "%s have %d apples.",
+	i18n.Fprintf(os.Stderr, `%s have %d apple.`, name, 2, i18n.Plural(
+		`%[2]d=1`, `%s have an apple.`,
+		`%[2]d=2`, `%s have two apples.`,
+		`%[2]d>2`, `%s have %d apples.`,
 	))
 	fmt.Println()
 }
